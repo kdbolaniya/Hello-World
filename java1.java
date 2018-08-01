5fa44fe6c168ee905f3e7c7c44862363b6c86b9d
KDB Bolaniya <kiranbolaniya9@gmail.com>
2018-07-31 22:48:02 -0700

    first commit

---
+++ java1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class java1
+{
+	public static void main(String args[])
+	{
+		int a[]={1,2,3,4,5,6,7,8,9};
+	
+		Scanner input=new Scanner(System.in);
+		int n=input.nextLine();
+		System.out.println("Number of Element"+n);
+		
+		for(int i=n;n>0;n--)
+		{
+			if(n>=k)
+			{
+				System.out.print(a[k+1]);
+				k++;
+			}
+			
+		}
+	}
+}
